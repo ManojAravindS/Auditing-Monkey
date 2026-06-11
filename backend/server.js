ffaaa5e2edc34f1b7fd30ef77e0fddb36c0db1f8
Manoj Aravind S <manojaravind1511@gmail.com>
2026-06-11 17:58:32 +0530

    Modification Made

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ io.on("connection", (socket) => {
     console.log("Client connected");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
